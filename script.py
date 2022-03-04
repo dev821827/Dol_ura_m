2f189b349ece6d691c71877d9fa6dcc14a58cf23
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-03-04 22:53:16 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,11 +13,11 @@ See <i>/help</i> for commands and more details.</b>
 
 
     HELP_MSG = """
-🚨 Oh! You Need Help!
+ Ohh❗You Need Help❗
 <b> Add me as Admin in your Group & Start Filtering!
 
 <b><u>Basic Commands</u></b>
-➠ /start - Check if I'm alive! 🟢
+➠ /start - Check if I'm alive!
 ➠ /help - Here You're!
 ➠ /about - To Know About Me!
 
@@ -25,7 +25,7 @@ See <i>/help</i> for commands and more details.</b>
 <b><u>Filter Commands</u></b>
 ➠ /add name reply - Add filter for name.
 ➠ /del name - Delete filter.
-➠ /delall - Delete entire filter database (☢️ Group Admin Only!)
+➠ /delall - Delete entire filter database (Group Admin Only)
 ➠ /viewfilters - List of all filters in chat.
 
 <b><u>Connection Commands</u></b>
@@ -35,7 +35,7 @@ See <i>/help</i> for commands and more details.</b>
 
 <b><u>Extras</u></b>
 
-➠ /status - Show Current Status of your Bot (🐝 Auth User Only!)
+➠ /status - Show Current Status of your Bot (Auth User Only)
 ➠ /id - Show ID information.
 ➠ /info userid - Show User Information. Use '/info' as reply to some message for their details!
 
